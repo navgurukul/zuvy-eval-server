7a63967dba84165bada8dc2df7e9cf76748f29a3
Numanazad23 <namanajid54a@gmail.com>
2026-03-02 15:35:49 +0530

    add swagger examples as well

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,14 +1,22 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { QuestionsService } from './questions.service';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { UpdateQuestionDto } from './dto/update-question.dto';
 import { GenerateQuestionsDto } from './dto/generate-questions.dto';
+import { generateQuestionsExample } from './swagger_examples/examples';
 
+@ApiTags('Questions')
 @Controller('questions')
 export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
 
   @Post('generate')
+  @ApiOperation({ summary: 'Enqueue background question generation jobs' })
+  @ApiBody({
+    type: GenerateQuestionsDto,
+    examples: generateQuestionsExample,
+  })
   async enqueueGeneration(@Body() payload: GenerateQuestionsDto) {
     return this.questionsService.enqueueGeneration(payload);
   }
---
+++ src/questions/swagger_examples/examples.ts
@@ -0,0 +1,31 @@
+export const generateQuestionsExample = {
+  basicExample: {
+    summary: 'Generate questions for a single topic',
+    value: {
+      domainName: 'Web Development',
+      topicName: 'REST APIs',
+      topicDescription: 'Async and Await calls',
+      numberOfQuestions: 5,
+      learningObjectives: 'To understand asynchronous API calls',
+      targetAudience: 'Beginner students',
+      focusAreas: 'Focus on calling REST APIs',
+      bloomsLevel: 'apply',
+      questionStyle: 'practical',
+      difficultyDistribution: {
+        easy: 11,
+        medium: 44,
+        hard: 45,
+      },
+      questionCounts: {
+        easy: 1,
+        medium: 2,
+        hard: 2,
+      },
+      topics: {
+        'REST APIs': 5,
+      },
+      levelId: null,
+    },
+  },
+};
+
